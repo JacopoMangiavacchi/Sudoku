7bf6cdbf660329dfc2b0ffe14ef19437443654be
Jacopo <jmangia@me.com>
2017-10-29 16:42:42 -0700

    Cleaner BFS brute force algorithm

--- Sudoku.playground/Contents.swift
+++ Sudoku.playground/Contents.swift
@@ -1,30 +1,48 @@
 import Foundation
 
+extension Sequence {
+    func eachSlice(_ clump:Int) -> [[Self.Element]] {
+        return self.reduce(into:[]) { memo, cur in
+            if memo.count == 0 {
+                return memo.append([cur])
+            }
+            if memo.last!.count < clump {
+                memo.append(memo.removeLast() + [cur])
+            } else {
+                memo.append([cur])
+            }
+        }
+    }
+}
+
+
 struct Sudoku {
-    private(set) var solutionMatrix: [[Int]]
-    private(set) var initialMatrix: [[Int]]
-    var workingMatrix: [[Int]]
+    private(set) var solutionMatrix: [Int]
+    private(set) var initialMatrix: [Int]
+    var workingMatrix: [Int]
     
-    init(size: Int, percent: Float = 0.44) {
-        let matrix = [[Int]](repeating: [Int](repeating: 0, count: size), count: size)
+    init(base: Int, percent: Float = 0.44) {
+        let matrix = [Int](repeating: 0, count: base*base)
         
         self.workingMatrix = matrix
         self.solutionMatrix = matrix
         self.initialMatrix = matrix
-        self.solve(random: true)
-        self.solutionMatrix = workingMatrix
-        self.clear(percent: percent)
-        self.initialMatrix = workingMatrix
+        if self.solve(random: true) {
+            self.solutionMatrix = workingMatrix
+            self.clear(percent: percent)
+            self.initialMatrix = workingMatrix
+        }
     }
     
     internal mutating func clear(percent: Float) {
         let size = workingMatrix.count
+        let base = Int(sqrt(Double(size)))
         let square = 3
         
-        for row in stride(from: 0, to: size, by: square) {
-            for col in stride(from: 0, to: size, by: square) {
-                let rowLenght = row+square > size ? size-row : square
-                let colLenght = col+square > size ? size-col : square
+        for row in stride(from: 0, to: base, by: square) {
+            for col in stride(from: 0, to: base, by: square) {
+                let rowLenght = row+square > base ? base-row : square
+                let colLenght = col+square > base ? base-col : square
                 let squareSize = rowLenght * colLenght
                 let toHide = Int(Float(squareSize) * percent)
                 var hidePositions = [Int]()
@@ -39,7 +57,8 @@ struct Sudoku {
                 for posToHide in hidePositions {
                     let rowToHide = row + (posToHide / colLenght)
                     let colToHide = col + (posToHide % colLenght)
-                    workingMatrix[rowToHide][colToHide] = 0
+                    
+                    workingMatrix[(rowToHide*base) + colToHide]
                 }
             }
         }
@@ -47,129 +66,114 @@ struct Sudoku {
         self.initialMatrix = workingMatrix
     }
     
+    internal func checkInArray(_ candidate: Int, array: [Int]) -> Bool {
+        for v in array {
+            if v == candidate {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func checkCandidate(_ candidate: Int, position: Int, base: Int, solvedMatrix: [Int]) -> Bool {
+        let row = position / base
+        let col = position - (row * base)
+        
+        var rowValues = [Int]()
+        var colValues = [Int]()
+        var squareValues = [Int]()
+        
+        for c in 0..<base {
+            let p = (row * base) + c
+            if workingMatrix[p] > 0 {
+                rowValues.append(workingMatrix[p])
+            }
+            else if solvedMatrix[p] > 0 {
+                rowValues.append(solvedMatrix[p])
+            }
+        }
+
+        for r in 0..<base {
+            let p = (r * base) + col
+            if workingMatrix[p] > 0 {
+                colValues.append(workingMatrix[p])
+            }
+            else if solvedMatrix[p] > 0 {
+                colValues.append(solvedMatrix[p])
+            }
+        }
+        
+        //TODO: squareValues
+        
+        if checkInArray(candidate, array: rowValues) || checkInArray(candidate, array: colValues) || checkInArray(candidate, array: squareValues) {
+            return false
+        }
+        
+        return true
+    }
+    
     mutating func solve(random: Bool = false) -> Bool {
         let size = workingMatrix.count
-        guard size > 0 && (workingMatrix.map{$0.count}.filter{$0 == size}.count == size) else { return false }
+        guard size > 0 else { return false }
+        let base = Int(sqrt(Double(size)))
         
-        var candidateMatrix: [[[Int]]] = [[[Int]]](repeating: [[Int]](repeating: [Int](), count: size), count: size) //candidateMatrix[row][col][candidate]
-        var solvedMatrix: [[Int]] = [[Int]]()
+        var candidateMatrix: [[Int]] = [[Int]](repeating: [Int](), count: size) // if random array of candidate used - else 1 only element is the last candidate used
+        var solvedMatrix: [Int] = [Int](repeating: 0, count: size)
         
-        while solvedMatrix.count < size {
-            var row = solvedMatrix.count
-            
-            var thisRow = [Int]()
-            while thisRow.count < size {
-                var col = thisRow.count
-                
-                var thisCol = [Int]()
-                for r in 0..<row {
-                    if solvedMatrix[r][col] > 0 {
-                        thisCol.append(solvedMatrix[r][col])
-                    }
-                }
-                for r in row..<size {
-                    thisCol.append(workingMatrix[r][col])
-                }
-                
-                if workingMatrix[row][col] == 0 {
-                    var rowsCandidateUsed = thisRow
-                    for c in workingMatrix[row] {
-                        if c > 0 && !rowsCandidateUsed.contains(c) {
-                            rowsCandidateUsed.append(c)
+        var pos = 0
+        while pos < size {
+            if workingMatrix[pos] == 0 {
+                var candidate = 0
+                if random {
+                    if candidateMatrix[pos].count < base {
+                        candidate = Int(arc4random_uniform(UInt32(base))+1)
+                        while candidateMatrix[pos].contains(candidate) {
+                            candidate = Int(arc4random_uniform(UInt32(base))+1)
                         }
-                    }
-                    for c in candidateMatrix[row][col] {
-                        if !rowsCandidateUsed.contains(c) {
-                            rowsCandidateUsed.append(c)
-                        }
-                    }
-                    for c in thisCol {
-                        if c > 0 && !rowsCandidateUsed.contains(c) {
-                            rowsCandidateUsed.append(c)
-                        }
-                    }
-                    
-                    if rowsCandidateUsed.count < size {
-                        var candidate = 0
-                        if random {
-                            candidate = Int(arc4random_uniform(UInt32(size))+1)
-                            while rowsCandidateUsed.contains(candidate) {
-                                candidate = Int(arc4random_uniform(UInt32(size))+1)
-                            }
-                        }
-                        else {
-                            rowsCandidateUsed.sort()
-                            
-                            for potentialCandidate in rowsCandidateUsed {
-                                if potentialCandidate - candidate > 1 {
-                                    break
-                                }
-                                candidate = potentialCandidate
-                            }
-                            
-                            candidate += 1
-                        }
-                        
-                        thisRow.append(candidate)
-                    }
-                    else {
-                        if col > 0 {
-                            for c in stride(from: col, to: 0, by: -1) {
-                                let last = thisRow.removeLast()
-                                candidateMatrix[row][c].removeAll()
-                                candidateMatrix[row][c-1].append(last)
-                                
-                                if workingMatrix[row][c-1] == 0 {
-                                    break
-                                }
-                            }
-                            
-                            if thisRow.count == 0 && workingMatrix[row][0] > 0 {
-                                col = 0
-                            }
-                        }
-                        
-                        if col == 0 && row > 0 {
-                            candidateMatrix[row] = [[Int]](repeating: [Int](), count: size)
-                            thisRow = solvedMatrix.removeLast()
-                            row -= 1
-                            col = size - 1
-                            
-                            for c in stride(from: col, to: 0, by: -1) {
-                                let last = thisRow.removeLast()
-                                candidateMatrix[row][c].append(last)
-                                
-                                if workingMatrix[row][c] == 0 {
-                                    break
-                                }
-                            }
-                        }
+                        candidateMatrix[pos].append(candidate)
                     }
                 }
                 else {
-                    thisRow.append(workingMatrix[row][col])
+                    if candidateMatrix[pos].count == 0 {
+                        candidateMatrix[pos].append(1)
+                        candidate = 1
+                    }
+                    else {
+                        candidate = candidateMatrix[pos][0] + 1
+                        if candidate <= base {
+                            candidateMatrix[pos][0] = candidate
+                        }
+                        else {
+                            candidate = 0
+                        }
+                    }
+                }
+                
+                if candidate == 0 {
+                    repeat {
+                        solvedMatrix[pos] = 0
+                        candidateMatrix[pos] = [Int]()
+                        pos -= 1
+                    }
+                    while pos > 0 && workingMatrix[pos] > 0
+                    
+                    if pos < 0 {
+                        print("NO SOLUTION !!!")
+                        return false
+                    }
+                }
+                else if checkCandidate(candidate, position: pos, base: base, solvedMatrix: solvedMatrix) {
+                    solvedMatrix[pos] = candidate
+                    pos += 1
                 }
             }
-            solvedMatrix.append(thisRow)
-        }
-        
-        
-        let expTot = (size * (size+1) / 2)
-        for row in solvedMatrix {
-            if row.reduce(0, +) != expTot {
-                return false
+            else {
+                solvedMatrix[pos] = workingMatrix[pos]
+                pos += 1
             }
         }
-        for c in 0..<size {
-            var col = [Int]()
-            for r in solvedMatrix {
-                col.append(r[c])
-            }
-            if col.reduce(0, +) != expTot {
-                return false
-            }
-        }
-        
+            
         workingMatrix = solvedMatrix
         return true
     }
@@ -182,7 +186,7 @@ struct Sudoku {
     }
     
     func display(_ kindOfMatrix: KindOfMatrix = .solution) {
-        var matrix: [[Int]]!
+        var matrix: [Int]!
         
         switch(kindOfMatrix) {
         case .solution:
@@ -193,21 +197,22 @@ struct Sudoku {
             matrix = workingMatrix
         }
         
-        matrix.map{$0.map{$0 > 0 ? String($0) : " "}.joined(separator: " ")}.forEach{print($0)}
+        matrix.eachSlice(Int(sqrt(Double(matrix.count)))).map{$0.map{$0 > 0 ? String($0) : "?"}.joined(separator: " ")}.forEach{print($0)}
     }
 }
 
 var startTime = CFAbsoluteTimeGetCurrent()
-var s = Sudoku(size: 9, percent: 0.5)
+var s = Sudoku(base: 9, percent: 0.5)
 var timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
 print("Time elapsed for Generating: \(timeElapsed) s.")
 
-//s.display()
-//print("")
-//s.display(.initial)
-//print("")
-//s.display(.working)
-//print("")
+s.display()
+print("")
+
+s.display(.initial)
+print("")
+s.display(.working)
+print("")
 
 let backup = s.workingMatrix
 startTime = CFAbsoluteTimeGetCurrent()
@@ -221,6 +226,6 @@ print(s.solve(random: true))
 timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
 print("Time elapsed for Solving Random: \(timeElapsed) s.")
 
-//print("")
-//s.display(.working)
+print("")
+s.display(.working)
 
